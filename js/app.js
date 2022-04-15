7ec4894a0c579a760fbd88ed6fb32225696e96d8
Z <98436673+theletter7a@users.noreply.github.com>
2022-04-15 13:35:44 -0400

    static

--- js/app.js
+++
@@ -1,6 +0,0 @@
-fetch('posts.json').then(res=>res.json()).then(data=>{
-    data.forEach(e=>{
-        var element = new XYZPost(e);
-        element.append(document.getElementById('content'));
-    });
-});
